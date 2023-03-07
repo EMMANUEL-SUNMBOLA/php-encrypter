110ab55dcb455e4716f280f2923058a31dabc949
Emmanuel Adedayo <adedayoemmanuel729@gmail.com>
2023-03-07 03:39:18 +0100

    bikini bottom

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 <h3>PHP ENCRYPTER</h3>
 <p>I made this encoder for fun</p>
 <ul>
-<li>I used the ascii character table</li>
+<li>I used the ascii character table to convert each letter/symbol/digit of the inputed string to it's corresponding ascii digits </li>
+<li>Already works in the terminal ,almost done with web version</li>
 </ul>
--- index.php
+++ index.php
@@ -1,15 +1,39 @@
 <?php
 
-$out = "";
-    $inp = readline("input for encoding in ascii chr...");
-    if(!empty($inp)){
-        for ($i=0; $i < strlen($inp); $i++) { 
-            $lin = substr($inp,$i);
-            $out .= ord($lin) . chr(0+rand(0,225));
+    if((isset($_POST["enc"])) && ($_SERVER["REQUEST_METHOD"] == "POST")){
+
+        $out = "";
+        // $inp = readline("input for encoding in ascii chr...");
+        $inp = $_POST["inp"];
+        if(!empty($inp)){
+            for ($i=0; $i < strlen($inp); $i++) { 
+                $lin = substr($inp,$i);
+                $out .= ord($lin) . chr(0+rand(0,225));
+            }
+            echo $out;
+        }else{
+            echo "don't leave input empty you doughnut";
         }
-        echo $out;
-    }else{
-        echo "don't leave empty you doughnut";
+
     }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="formdiv">
+        <form action="" method="post">
+            <input type="text" name="inp"><br>
+            <button type="submit" name="enc">ENCRYPT</button> 
+            <button type="submit" name="dec">DECRYPT</button> 
+        </form>
+    </div>
+</body>
+</html>
